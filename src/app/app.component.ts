48595d22abbd3f4abdb46af392deda5b68a0a009
RinzlerN26 <>
2025-02-08 19:05:48 +0530

    Redux Setup Complete.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RootState } from "../app/store";
+import { injectSelector, injectDispatch } from "@reduxjs/angular-redux";
+import { decrement, increment } from "../app/reducer/userSlice";
 
 @Component({
   selector: 'app-root',
@@ -9,4 +12,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Pharmserv-UI';
+  count = injectSelector((state: RootState) => state.user.value);
+  dispatch = injectDispatch();
+  increment = increment;
+  decrement = decrement;
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideRedux } from "@reduxjs/angular-redux";
 import { routes } from './app.routes';
+import store  from "../app/store";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideRedux({ store })]
 };
