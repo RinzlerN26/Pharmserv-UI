ec2940082beeef28d05eca920010b0879dffecfc
RinzlerN26 <>
2025-02-22 15:04:27 +0530

    Added Header Component and Routes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
 import { RouterOutlet } from '@angular/router';
-import { AppState } from './reducer';
-import { increment, decrement, reset } from './actions/counter.actions'; // Import your actions
-import { Store } from '@ngrx/store';
 import { CommonModule } from '@angular/common';
-import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  imports: [CommonModule, HeaderComponent, RouterOutlet],
+  template: `<app-header></app-header> <router-outlet></router-outlet>`,
 })
-export class AppComponent {
-  title = 'PharmServ';
-
-  count$: Observable<number>;
-
-  constructor(private store: Store<AppState>) {
-    this.count$ = this.store.select((state) => state.counter.count);
-  }
-
-  increment() {
-    this.store.dispatch(increment());
-  }
-
-  decrement() {
-    this.store.dispatch(decrement());
-  }
-
-  reset() {
-    this.store.dispatch(reset());
-  }
-}
+export class AppComponent {}
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from '../../reducer';
+import { increment, decrement, reset } from '../../actions/counter.actions';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {
+  title = 'PharmServ';
+  count$: Observable<number>;
+
+  constructor(private store: Store<AppState>) {
+    this.count$ = this.store.select((state) => state.counter.count);
+  }
+
+  increment() {
+    this.store.dispatch(increment());
+  }
+
+  decrement() {
+    this.store.dispatch(decrement());
+  }
+
+  reset() {
+    this.store.dispatch(reset());
+  }
+}
