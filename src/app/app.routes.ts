42bbab7426fdb8763cd262ef6d2f9edc68fd0631
RinzlerN26 <>
2025-03-22 20:15:32 +0530

    Created Pharma Component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SigninComponent } from './pages/signin/signin.component';
+import { PharmaComponent } from './pages/pharma/pharma.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'signin', component: SigninComponent },
+  { path: 'pharma', component: PharmaComponent },
 ];
---
+++ src/app/pages/pharma/pharma.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pharma',
+  imports: [],
+  templateUrl: './pharma.component.html',
+  styleUrl: './pharma.component.scss'
+})
+export class PharmaComponent {
+
+}
