e9bda726188fa0ad775f97bc6e34dad2f70751a7
RinzlerN26 <>
2025-03-23 18:33:15 +0530

    Auth Guard Changes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SigninComponent } from './pages/signin/signin.component';
 import { PharmaComponent } from './pages/pharma/pharma.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'signin', component: SigninComponent },
-  { path: 'pharma', component: PharmaComponent },
+  { path: 'pharma', component: PharmaComponent, canActivate: [authGuard] },
 ];
