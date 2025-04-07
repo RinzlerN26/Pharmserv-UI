2b55d44cb6ca0a0cd1b971f58b904a8ff3f84584
RinzlerN26 <nishantonnet45@gmail.com>
2025-04-07 18:22:28 +0530

    Refactoring Changes.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { AppState } from '../../reducer';
-import { increment, decrement, reset } from '../../actions/counter.actions';
 
 @Component({
   selector: 'app-home',
@@ -14,21 +13,6 @@ import { increment, decrement, reset } from '../../actions/counter.actions';
 })
 export class HomeComponent {
   title = 'PharmServ';
-  count$: Observable<number>;
 
-  constructor(private store: Store<AppState>) {
-    this.count$ = this.store.select((state) => state.counter.count);
-  }
-
-  increment() {
-    this.store.dispatch(increment());
-  }
-
-  decrement() {
-    this.store.dispatch(decrement());
-  }
-
-  reset() {
-    this.store.dispatch(reset());
-  }
+  constructor(private store: Store<AppState>) {}
 }
--- src/app/reducer.ts
+++ src/app/reducer.ts
@@ -1,15 +1,10 @@
 import { ActionReducerMap } from '@ngrx/store';
-import { counterReducer } from '../app/reducers/counter.reducer';
 import { userReducer, UserState } from './reducers/user.reducer';
 
 export interface AppState {
-  counter: {
-    count: number;
-  };
   user: UserState;
 }
 
 export const reducers: ActionReducerMap<AppState> = {
-  counter: counterReducer,
   user: userReducer,
 };
